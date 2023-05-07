1d39622a92cc3627c780437c921779cc614e44d9
Marssssssss <3503715604@qq.com>
2023-05-07 23:19:39 +0800

    delete useless files

--- v3/data/download_data.bat
+++
@@ -1,2 +0,0 @@
-curl -o mnist_test.csv https://pjreddie.com/media/files/mnist_test.csv
-curl -o mnist_train.csv https://pjreddie.com/media/files/mnist_train.csv
--- v3/image_reader.bat
+++
@@ -1,1 +0,0 @@
-python image_reader.py
